81db98855be7c6dd47e4e018d1739a21b84e3269
Alexandre Hoss <alexandre.hoss@yahoo.com.br>
2022-06-19 18:05:12 -0300

    Atividade do encontro remoto 02

---
+++ Encontro Remoto 02/Atividade par impar.js
@@ -0,0 +1,18 @@
+// Conside a quantidade de alunos presentes na sala, percorra do zero até o número total de alunos 
+// se o número for par, escreva par e o numero correspondente
+// se o número for impar, escreca impar e o numero correspondente
+// se o número for zero, escreva zero e o número correspondente
+
+let alunospresentes=17;
+
+for (let aluno = 0; aluno <= alunospresentes; aluno++) {
+
+    if(aluno == 0){
+        console.log("zero " + aluno);
+    }
+    if(aluno % 2 == 0){//par
+        console.log("par " + aluno)
+    } else {
+        console.log("impar " + aluno);
+    }
+}
